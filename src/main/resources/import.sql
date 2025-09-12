5b794b698ac333f3d0fc4dc0828a92ad087baf45
KauanBS-art <kauan07.kbs@gmail.com>
2025-09-12 08:41:10 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,11 +5,3 @@ INSERT INTO jogo (titulo, descricao, preco, dataLancamento)
 VALUES ('Elden Ring', 'Um soulslike de fantasia criado por Hidetaka Miyazaki e George R.R. Martin.', 249.90, '2022-02-25');
 INSERT INTO jogo (titulo, descricao, preco, dataLancamento) 
 VALUES ('Minecraft', 'Um jogo sandbox de blocos infinitos.', 79.90, '2011-11-18');
-
--- Usuários
-INSERT INTO usuario (nome, email, senha, tipo)
-VALUES ('Kauan Batista Silveira', 'kauansenior@gmail.com', '123456', 'ADMIN');
-INSERT INTO usuario (nome, email, senha, tipo)
-VALUES ('Maria Therezza de Oliveira', 'mariatherezza15@gmail.com', 'hellokitty', 'CLIENTE');
-INSERT INTO usuario (nome, email, senha, tipo)
-VALUES ('Edson Arantes do Nascimento', 'josoareslenda@gmail.com', 'pelerei', 'CLIENTE');
