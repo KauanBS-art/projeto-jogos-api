e61944507b67cefa56eba20bc16c1c0c362bbf93
KauanBS-art <kauan07.kbs@gmail.com>
2025-11-06 23:41:11 -0300

    Add transactional support and error handling
    Added transactional annotation to methods for database operations and improved error handling in the create method.

--- src/main/java/com/kauangamestore/resource/EmpresaResource.java
+++ src/main/java/com/kauangamestore/resource/EmpresaResource.java
@@ -6,13 +6,10 @@ import com.kauangamestore.dto.EmpresaDTO;
 import com.kauangamestore.dto.EmpresaDTOResponse;
 import com.kauangamestore.service.EmpresaService;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriBuilder;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.core.*;
 
 import java.util.List;
 
@@ -34,21 +31,27 @@ public class EmpresaResource {
     @Path("/{id}")
     public Response findById(@PathParam("id") Long id) {
         EmpresaDTOResponse empresa = empresaService.findById(id);
-        if (empresa == null) {
+        if (empresa == null)
             return Response.status(Response.Status.NOT_FOUND).build();
-        }
         return Response.ok(empresa).build();
     }
 
     @POST
+    @Transactional
     public Response criar(@Valid EmpresaDTO dto, @Context UriInfo uriInfo) {
         EmpresaDTOResponse created = empresaService.create(dto);
+        if (created == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Erro ao criar empresa. Verifique os dados enviados.")
+                    .build();
+        }
         UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(String.valueOf(created.getId()));
         return Response.created(builder.build()).entity(created).build();
     }
 
     @PUT
     @Path("/{id}")
+    @Transactional
     public Response atualizar(@PathParam("id") Long id, @Valid EmpresaDTO dto) {
         EmpresaDTOResponse updated = empresaService.update(id, dto);
         return updated == null
@@ -56,9 +59,9 @@ public class EmpresaResource {
             : Response.ok(updated).build();
     }
 
-
     @DELETE
     @Path("/{id}")
+    @Transactional
     public Response delete(@PathParam("id") Long id) {
         empresaService.delete(id);
         return Response.status(Response.Status.NO_CONTENT).build();
