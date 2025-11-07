cd7dbb24e4a64f02216c74b0333de24a68952ad8
KauanBS-art <kauan07.kbs@gmail.com>
2025-11-06 23:47:18 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,6 @@ VALUES ('Elden Ring', 'Um soulslike de fantasia criado por Hidetaka Miyazaki e G
 INSERT INTO jogo (titulo, descricao, preco, dataLancamento, id_empresa)
 VALUES ('Minecraft', 'Um jogo sandbox de blocos infinitos.', 79.90, '2011-11-18', 1);
 
-
 -- Categorias
 INSERT INTO jogo_categoria (id_jogo, id_categoria)
 VALUES (1, 1);
@@ -22,3 +21,11 @@ INSERT INTO jogo_plataforma (id_jogo, id_plataforma)
 VALUES (2, 2);
 INSERT INTO jogo_plataforma (id_jogo, id_plataforma)
 VALUES (3, 3);
+
+-- Empresas
+INSERT INTO empresa (id, nome, descricao, paisOrigem)
+VALUES (1, 'CD Projekt', 'Desenvolvedora polonesa conhecida pela franquia The Witcher', 'Polônia');
+INSERT INTO empresa (id, nome, descricao, paisOrigem)
+VALUES (2, 'FromSoftware', 'Estúdio japonês criador de Dark Souls, Bloodborne e Elden Ring', 'Japão');
+INSERT INTO empresa (id, nome, descricao, paisOrigem)
+VALUES (3, 'Mojang Studios', 'Desenvolvedora sueca responsável por Minecraft', 'Suécia');
