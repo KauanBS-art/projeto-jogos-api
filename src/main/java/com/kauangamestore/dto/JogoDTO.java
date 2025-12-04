7a4bbaa4f5658a23585fd2974cfcaab788e8ec63
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 20:45:22 -0300

    Update JogoDTO.java

--- src/main/java/com/kauangamestore/dto/JogoDTO.java
+++ src/main/java/com/kauangamestore/dto/JogoDTO.java
@@ -2,35 +2,31 @@
 
 package com.kauangamestore.dto;
 
-import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.util.List;
 
 public record JogoDTO(
-
-    @NotBlank(message = "O título do jogo deve ser informado.")
-    @Size(max = 120, message = "O título pode ter no máximo 120 caracteres.")
+    @NotBlank(message = "Título é obrigatório")
     String titulo,
 
-    @NotBlank(message = "A descrição do jogo deve ser informada.")
-    @Size(max = 255, message = "A descrição pode ter no máximo 255 caracteres.")
     String descricao,
 
-    @NotNull(message = "O preço deve ser informado.")
-    @Positive(message = "O preço deve ser maior que zero.")
+    @NotNull(message = "Preço é obrigatório")
+    @Positive
     Double preco,
 
-    @Schema(type = SchemaType.STRING, example = "09/08/2005")
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    @NotNull(message = "A data de lançamento deve ser informada.")
+    @NotNull(message = "Estoque é obrigatório")
+    @Positive
+    Integer estoque, // CAMPO NOVO IMPORTANTE
+
     LocalDate dataLancamento,
 
-    @NotNull(message = "A empresa do jogo deve ser informada (ID).")
-    Long idEmpresa
-) {}
+    @NotNull(message = "Empresa é obrigatória")
+    Long idEmpresa,
+
+    // Relacionamento N:N
+    List<Long> idPlataformas
+) { }
