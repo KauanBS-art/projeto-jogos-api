f64dc0a7122a2cb57802fd6430b3a096df75b2a0
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 20:48:33 -0300

    Update UsuarioDTO.java

--- src/main/java/com/kauangamestore/dto/UsuarioDTO.java
+++ src/main/java/com/kauangamestore/dto/UsuarioDTO.java
@@ -2,21 +2,21 @@
 
 package com.kauangamestore.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UsuarioDTO(
-
-    @NotBlank(message = "O nome do usuário deve ser informado.")
-    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
+    @NotBlank(message = "O nome é obrigatório")
     String nome,
 
-    @NotBlank(message = "O e-mail deve ser informado.")
-    @Email(message = "O e-mail informado é inválido.")
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
     String email,
 
-    @NotBlank(message = "A senha deve ser informada.")
-    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres.")
-    String senha
-) {}
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    String senha,
+
+    Integer idPerfil
+) { }
