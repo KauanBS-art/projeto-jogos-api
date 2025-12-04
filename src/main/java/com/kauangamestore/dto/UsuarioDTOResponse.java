c165e2613e38e938fe9e968ee0cb7658756cdf03
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 20:48:56 -0300

    Update UsuarioDTOResponse.java

--- src/main/java/com/kauangamestore/dto/UsuarioDTOResponse.java
+++ src/main/java/com/kauangamestore/dto/UsuarioDTOResponse.java
@@ -2,8 +2,21 @@
 
 package com.kauangamestore.dto;
 
+import com.kauangamestore.model.Perfil;
+import com.kauangamestore.model.Usuario;
+
 public record UsuarioDTOResponse(
     Long id,
     String nome,
-    String email
-) {}
+    String email,
+    Perfil perfil
+) {
+    public static UsuarioDTOResponse valueOf(Usuario usuario) {
+        return new UsuarioDTOResponse(
+            usuario.getId(),
+            usuario.getNome(),
+            usuario.getEmail(),
+            usuario.getPerfil()
+        );
+    }
+}
