d1848d413ae87049d9a9a572772d6d6ea3b8d25b
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 21:29:47 -0300

    Update EmpresaResourceTest.java

--- src/main/test/java/com/kauangamestore/resource/EmpresaResourceTest.java
+++ src/main/test/java/com/kauangamestore/resource/EmpresaResourceTest.java
@@ -1,60 +1,44 @@
+//Classe de Kauan Batista Silveira
+
 package com.kauangamestore.resource;
 
+import com.kauangamestore.dto.EmpresaDTO;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
 public class EmpresaResourceTest {
 
     @Test
-    void deveListarEmpresas() {
+    public void testFindAll() {
         given()
             .when().get("/empresas")
             .then()
-            .statusCode(200)
-            .body("size()", greaterThanOrEqualTo(0));
+            .statusCode(200);
     }
 
     @Test
-    void deveCriarAtualizarExcluirEmpresa() {
+    @TestSecurity(user = "admin", roles = {"ADMINISTRADOR"})
+    public void testCreate() {
+        EmpresaDTO dto = new EmpresaDTO(
+            "Sega",
+            "Japão",
+            "Criadora do Sonic"
+        );
 
-        // Criação com JSON na ordem correta
-        String location = given()
-            .contentType(ContentType.JSON)
-            .body("{"
-                + "\"nome\":\"FromSoftware\","
-                + "\"paisOrigem\":\"Japão\","
-                + "\"descricao\":\"Desenvolvedora japonesa criadora de Dark Souls e Elden Ring\""
-                + "}")
-        .when()
-            .post("/empresas")
-        .then()
-            .statusCode(201)
-            .extract().header("Location");
-
-        String id = location.substring(location.lastIndexOf('/') + 1);
-
-        // Atualização
         given()
             .contentType(ContentType.JSON)
-            .body("{"
-                + "\"nome\":\"FromSoftware Inc.\","
-                + "\"paisOrigem\":\"Japão\","
-                + "\"descricao\":\"Desenvolvedora japonesa de jogos de ação e fantasia\""
-                + "}")
-        .when()
-            .put("/empresas/" + id)
-        .then()
-            .statusCode(anyOf(is(200), is(204)));
-
-        // Exclusão
-        given()
-            .when().delete("/empresas/" + id)
+            .body(dto)
+            .when().post("/empresas")
             .then()
-            .statusCode(204);
+            .statusCode(201)
+            .body("id", notNullValue())
+            .body("nome", is("Sega"));
     }
 }
