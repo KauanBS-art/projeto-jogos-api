a495ffc8e45cfb3d3d155df5d5fba379ed47b119
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 21:37:39 -0300

    Update UsuarioResourceTest.java

--- src/main/test/java/com/kauangamestore/resource/UsuarioResourceTest.java
+++ src/main/test/java/com/kauangamestore/resource/UsuarioResourceTest.java
@@ -1,46 +1,47 @@
+//Classe de Kauan Batista Silveira
+
 package com.kauangamestore.resource;
 
+import com.kauangamestore.dto.UsuarioDTO;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 
 @QuarkusTest
 public class UsuarioResourceTest {
 
     @Test
-    void deveListarUsuarios() {
+    @TestSecurity(user = "admin", roles = {"ADMINISTRADOR"})
+    public void testFindAll() {
         given()
             .when().get("/usuarios")
             .then()
-            .statusCode(200)
-            .body("size()", greaterThanOrEqualTo(0));
+            .statusCode(200);
     }
 
     @Test
-    void deveCriarEDeletarUsuario() {
-        // Cria novo usuário
-        String location = given()
-            .contentType(ContentType.JSON)
-            .body("{"
-                + "\"nome\":\"João Tester\","
-                + "\"email\":\"joao@test.com\","
-                + "\"senha\":\"123456\""
-                + "}")
-        .when()
-            .post("/usuarios")
-        .then()
-            .statusCode(201)
-            .extract().header("Location");
+    public void testCreate() {
+        UsuarioDTO dto = new UsuarioDTO(
+            "Novo Usuario Teste",
+            "teste@email.com",
+            "123456",
+            2
+        );
 
-        String id = location.substring(location.lastIndexOf('/') + 1);
-
-        // Exclui o usuário criado
         given()
-            .when().delete("/usuarios/" + id)
-        .then()
-            .statusCode(204);
+            .contentType(ContentType.JSON)
+            .body(dto)
+            .when().post("/usuarios")
+            .then()
+            .statusCode(201)
+            .body("id", notNullValue())
+            .body("email", is("teste@email.com"))
+            .body("senha", nullValue()); 
     }
 }
