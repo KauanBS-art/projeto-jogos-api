d843a4cdaed34f3c4a74f999ea336efb39355fed
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 21:04:07 -0300

    Update Usuario.java

--- src/main/java/com/kauangamestore/model/Usuario.java
+++ src/main/java/com/kauangamestore/model/Usuario.java
@@ -6,7 +6,6 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "usuario")
 public class Usuario extends PanacheEntityBase {
 
     @Id
@@ -22,23 +21,33 @@ public class Usuario extends PanacheEntityBase {
     @Column(nullable = false)
     private String senha;
 
+
+    @Enumerated(EnumType.STRING)
+    private Perfil perfil;
+
+
     public Usuario() {}
 
-    public Usuario(String nome, String email, String senha) {
+    public Usuario(String nome, String email, String senha, Perfil perfil) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+        this.perfil = perfil;
     }
 
+    // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
+    
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
+    
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
+    
     public String getSenha() { return senha; }
     public void setSenha(String senha) { this.senha = senha; }
+    
+    public Perfil getPerfil() { return perfil; }
+    public void setPerfil(Perfil perfil) { this.perfil = perfil; }
 }
