46a89d775558a204f7a55642f79cecfe14931f77
KauanBS-art <kauan07.kbs@gmail.com>
2025-12-04 21:00:19 -0300

    Create PagamentoBoleto.java

---
+++ src/main/java/com/kauangamestore/model/PagamentoBoleto.java
@@ -0,0 +1,20 @@
+//Classe de Kauan Batista
+
+package com.kauangamestore.model;
+
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class PagamentoBoleto extends Pagamento {
+
+    private String numeroBoleto;
+    private LocalDate dataVencimento;
+  
+
+    public String getNumeroBoleto() { return numeroBoleto; }
+    public void setNumeroBoleto(String numeroBoleto) { this.numeroBoleto = numeroBoleto; }
+
+    public LocalDate getDataVencimento() { return dataVencimento; }
+    public void setDataVencimento(LocalDate dataVencimento) { this.dataVencimento = dataVencimento; }
+}
